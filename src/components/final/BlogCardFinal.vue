92f528d8542d86e29bf1564136a41adca9a9ac55
Alexander von Studnitz <studnitz@hey.com>
2021-03-01 16:49:43 +0100

    WIP

--- src/components/final/BlogCardFinal.vue
+++ src/components/final/BlogCardFinal.vue
@@ -1,6 +1,6 @@
 <template>
   <article
-    class="p-8 space-y-2 text-gray-800 transition transform bg-white rounded-lg shadow-lg cursor-pointer active:translate-y-0 hover:shadow-2xl hover:-translate-y-1"
+    class="p-8 space-y-4 text-gray-800 transition transform bg-white rounded-lg shadow-lg cursor-pointer active:translate-y-0 hover:shadow-2xl hover:-translate-y-1"
   >
     <header>
       <h3 class="text-xl font-bold">Ist die Erde wirklich eine Kugel?</h3>
@@ -11,7 +11,7 @@
       Mitte haben. Was in dieser Konstellation dann die Glasur sein soll, ist
       aktuell Forschungsgegenstand.
     </p>
-    <footer class="space-y-6">
+    <footer class="space-y-6 -rotat">
       <hr class="w-24 mx-auto" />
       <address class="italic text-gray-700">von Alexander von Studnitz</address>
     </footer>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import { createApp } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import App from "./App.vue"
-import Landing from "./pages/Landing.vue"
+import Landing from "./pages/LandingDemo.vue"
 import LandingFinal from "./pages/LandingFinal.vue"
+import LandingStart from "./pages/LandingStart.vue"
 import "./index.css"
 
 const router = createRouter({
   routes: [
     { path: "/", component: Landing },
     { path: "/final", component: LandingFinal },
+    { path: "/start", component: LandingStart },
   ],
   history: createWebHistory(),
 })
